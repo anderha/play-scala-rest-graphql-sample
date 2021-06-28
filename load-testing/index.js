cc51b28e3485dced5ea5b90e61082bd87b8f3182
Alexander Hauenstein <a.hauenstein@innfactory.de>
2021-06-28 17:03:31 +0200

    add comments

--- load-testing/index.js
+++ load-testing/index.js
@@ -4,9 +4,10 @@ const fs = require('fs')
 const path = require('path')
 const LoadTesting = require('easygraphql-load-tester')
 
-const todoSchema = fs.readFileSync(path.join(__dirname, './ressources/schema.graphql'), 'utf8')
+const todoSchema = fs											// read the schema file into a variable
+	.readFileSync(path.join(__dirname, './ressources/schema.graphql'), 'utf8')
 
-const args = {
+const args = {													// arguments to use on queries/mutations which require input
 	todo: {
 		id: 1,
 	},
@@ -18,17 +19,17 @@ const args = {
 	}
 }
 
-const customQueries = []
+const customQueries = [] 										// define custom graphql queries
 
-const easyGraphQLLoadTester = new LoadTesting(todoSchema, args)
+const easyGraphQLLoadTester = new LoadTesting(todoSchema, args)	// defines the settings for the load test, explicitly the graphql schema and the arguments
 
-const runQueries = easyGraphQLLoadTester.artillery({
+const runQueries = easyGraphQLLoadTester.artillery({ 			// define a load test using artillery
   customQueries,
-  onlyCustomQueries: false,
-  queryFile: false,
-  withMutations: true,
+  onlyCustomQueries: false,										// if true it runs only custom queries
+  queryFile: false, 											// creates a file with all the queries created for the load test
+  withMutations: true, 											// defines if graphql mutation queries are used by the load test
 })
 
 module.exports = {
-	runQueries,
+	runQueries,													// export the function runQueries to use within other modules
 }
